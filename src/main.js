7201f36e68379110575fa38e330fbea288571508
yyyflowers <619873609@qq.com>
2021-09-02 16:04:05 +0800

    '提交'

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 引用音频播放插件
-import vueAplayer from 'vue-aplayer'
-Vue.use(vueAplayer) ;
 
 // import style
 import 'view-design/dist/styles/iview.css';
